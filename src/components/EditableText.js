2a691defd7f80e16cf050878ab7a7e336edaeb48
Mclilzee <mc.lilzee@hotmail.com>
2022-05-06 00:18:12 +0200

    EditableText component control it's own state

--- src/components/EditableText.js
+++ src/components/EditableText.js
@@ -2,12 +2,22 @@ import React from "react";
 
 export default function EditableText(props) {
 
+    const [isSelected, setIsSelected] = React.useState(false);
+
+    function handleClick() {
+        setIsSelected(true);
+    }
+
+    function handleBlur() {
+        setIsSelected(false);
+    }
+
     function getInputType() {
-        if (props.data.isSelected) {
-            return <input className={props.className} type={"text"} value={props.data.text} id={props.data.id}
+        if (isSelected) {
+            return <input autoFocus={true} onBlur={handleBlur} className={props.className} type={"text"} value={props.data.text} id={props.data.id}
                           name={"inputText"}/>;
         } else {
-            return <div className={props.className} id={props.data.id}>{props.data.text}</div>;
+            return <div onClick={handleClick} onBlur={handleBlur} className={props.className} id={props.data.id}>{props.data.text}</div>;
         }
     }
 
