f253e81ea49c7ad7822ed42d5650d884d8d922f5
Mclilzee <mc.lilzee@hotmail.com>
2022-05-06 02:54:31 +0200

    Merge content-editable

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,25 @@
 import React from "react";
-import EditableText from "./EditableText";
 import Image from "../assets/images/photo-placeholder.jpg"
 import PersonalDetail from "./PersonalDetail";
 import addButton from "../assets/buttons/add-button.svg"
 
-export default function Header({fullName, personalData}) {
+export default function Header(props) {
 
-    const details = personalData.map((item) => {
-        return <PersonalDetail key={item.personalTitle.id} title={item.personalTitle} detail={item.personalDetail}/>
-    })
+    const [fullName, setFullName] = React.useState(() => "");
+
+
+    function handleNameChange(event) {
+        let newName = event.target.value;
+        setFullName(newName)
+    }
 
     return (
         <div className="header">
-            <EditableText className="fullName" data={fullName}/>
+            <input type="text" placeholder={"Full Name"} onChange={handleNameChange} className="fullName"
+                   value={fullName}/>
             <img src={Image} alt={"person"}/>
             <div className={"personal-information"}>
-                {details}
+                <PersonalDetail/>
                 <img src={addButton} alt={"add button"}/>
             </div>
             <div className={"line-break"}/>
--- src/components/PersonalDetail.js
+++ src/components/PersonalDetail.js
@@ -1,12 +1,46 @@
 import React from "react";
-import EditableText from "./EditableText";
 
 export default function PersonalDetail(props) {
 
-    return (
-        <div className={"personal-detail"}>
-            <EditableText className={"title"} data={props.title} isSelected={false}/>
-            <EditableText className={"detail"} data={props.detail} isSelected={false}/>
+    const [personalData, setPersonalData] = React.useState(() => [
+        {
+            title: "Address",
+            detail: "Berlin Str. 221",
+        },
+        {
+            title: "Phone :",
+            detail: "+49 153 232212"
+        },
+        {
+            title: "E-mail :",
+            detail: "example@hotmail.com"
+        }
+    ]);
+
+    function handleChange(event) {
+        const index = Number(event.target.id);
+        const text = event.target.value;
+        const type = event.target.className;
+
+        setPersonalData(prevData => {
+            return prevData.map((item, itemIndex) => {
+                return itemIndex === index ? {...item, [type]: text} :
+                    {...item}
+            })
+        })
+    }
+
+
+    const informationArray = personalData.map((item, index) => {
+        return <div id={index} className={"personal-detail"}>
+            <input placeholder={"Title"} id={index} onChange={handleChange} className={"title"} value={item.title}/>
+            <input placeholder={"Detail"} id={index} onChange={handleChange} className={"detail"} value={item.detail}/>
         </div>
+    })
+
+    return (
+        <>
+            {informationArray}
+        </>
     )
 }
