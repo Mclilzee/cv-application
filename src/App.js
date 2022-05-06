a0797ed901703d9159776f7ef114206b84bf3bea
Mclilzee <mc.lilzee@hotmail.com>
2022-05-06 12:52:05 +0200

    Add event listener for keyboard

--- src/App.js
+++ src/App.js
@@ -4,9 +4,38 @@ import React from "react";
 
 function App() {
 
+    React.useEffect(() => {
+
+        function handleKeyPress(event) {
+
+            if (event.key !== "Escape" && event.key !== "Enter") {
+                return;
+            }
+
+            if (event.key === "Escape") {
+                const textAreas = document.querySelectorAll("textarea");
+                textAreas.forEach(textArea => textArea.blur());
+            }
+
+            blurInputs()
+
+        }
+
+        function blurInputs() {
+            const inputs = document.querySelectorAll("input");
+            inputs.forEach(input => input.blur())
+        }
+
+        document.addEventListener("keydown", handleKeyPress);
+
+        return () => {
+            document.removeEventListener("keydown", handleKeyPress);
+        }
+    }, [])
+
     return (
         <div className="paper">
-            <Header />
+            <Header/>
         </div>
     );
 }
