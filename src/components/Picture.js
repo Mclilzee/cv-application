19afe42c0de40b9f2252e33c9a4d30839e6a8699
Mclilzee <mc.lilzee@hotmail.com>
2022-05-07 21:10:47 +0200

    Finish form styling and add page sizes

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -16,6 +16,7 @@ export default function Picture(props) {
         console.log(new Image())
         if (imageUrl.match(/\.(jpeg|jpg|png)$/)) {
             setProfilePicture(imageUrl)
+            setShowForm(false)
         } else {
             setPasteImageUrlMessage("Invalid Link, Please choose a valid image link.")
         }
@@ -31,23 +32,22 @@ export default function Picture(props) {
         setShowForm(false);
     }
 
-    const formStyle = {
-        visibility: showForm ? "" : "hidden"
-    }
-
 
     return (
         <div className={"picture-container"}>
-            <div style={formStyle} className={"form-container"}>
-                <form onSubmit={chooseImageURL}>
-                    <img src={closeButton} alt={"close form button"} onClick={handleCloseFormButton}
-                         className={"close-button"}/>
-                    <label form={"url-input"}>{pasteImageUrlMessage}</label>
-                    <input id="url-input" type="url" name="url"/>
-                    <button type="submit">Select</button>
-                    <a href={"https://imgur.com"} target={"_blank"} rel="noreferrer">Upload your Image using Imgur</a>
-                </form>
-            </div>
+            {showForm &&
+                <div className={"form-container"}>
+                    <form onSubmit={chooseImageURL}>
+                        <img src={closeButton} alt={"close form button"} onClick={handleCloseFormButton}
+                             className={"close-button"}/>
+                        <label form={"url-input"}>{pasteImageUrlMessage}</label>
+                        <input placeholder={"Example: https//i.imgur.com/example.jpeg"} id="url-input" type="url"
+                               name="url"/>
+                        <button type="submit">Select</button>
+                        <a href={"https://imgur.com/upload"} target={"_blank"} rel="noreferrer">Upload using Imgur</a>
+                    </form>
+                </div>
+            }
             <img onClick={handleImageClick} className={"profile-picture"} src={profilePicture} alt={"person"}/>
         </div>
     )
